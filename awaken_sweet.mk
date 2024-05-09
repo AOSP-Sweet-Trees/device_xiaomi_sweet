35c0be9936bd0f7ac57d6227cdef6c70bc8c7070
K R I S H N A <krishnakripa34567@gmail.com>
2024-05-09 13:23:34 +0000

    sweet: welcome AwakenOS

--- awaken_sweet.mk
+++ awaken_sweet.mk
@@ -13,16 +13,25 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
 # Inherit some common Project Elixir stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/awaken/config/common_full_phone.mk)
 
-# Elixir Officials
+
+# Boot Animation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_SUPPORTS_QUICK_TAP := true
-ELIXIR_BUILD_TYPE := OFFICIAL
-TARGET_CALL_RECORDING_SUPPORTED := true
+
+# Google Recorder
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+
+# Google Assistant
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+
+#Pixel Charger
+USE_PIXEL_CHARGER := true
+
+#face unlock
 TARGET_FACE_UNLOCK_SUPPORTED := true
 
-PRODUCT_NAME := aosp_sweet
+PRODUCT_NAME := awaken_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
