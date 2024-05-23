9a8ff51ba3dd7dc5c4e561b0b66cd98ae7fb2e27
K R I S H N A <krishnakripa34567@gmail.com>
2024-05-23 20:04:16 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -18,3 +18,30 @@ git clone https://github.com/ProjectElixir-Devices/hardware_xiaomi hardware/xiao
 rm -rf kernel/xiaomi
 git clone --depth=1 https://github.com/pure-soul-kk/kernel_xiaomi_sm6150 kernel/xiaomi/sm6150
 
+rm -rf packages/modules/Bluetooth
+git clone --depth=1 https://github.com/AOSP-Sweet-Trees/packages_modules_Bluetooth packages/modules/Bluetooth
+
+rm -rf frameworks/base
+git clone --depth=1 https://github.com/AOSP-Sweet-Trees/frameworks_base frameworks/base
+
+rm -rf frameworks/av
+git clone --depth=1 https://github.com/AOSP-Sweet-Trees/frameworks_av frameworks/av 
+
+rm -rf packages/apps/Settings
+git clone --depth=1 https://github.com/AOSP-Sweet-Trees/packages_apps_Settings packages/apps/Settings
+
+rm -rf vendor/pixys
+git clone --depth=1 https://github.com/AOSP-Sweet-Trees/vendor_pixys vendor/pixys
+
+rm -rf device/pixys/sepolicy
+git clone https://github.com/AOSP-Sweet-Trees/device_pixys_sepolicy device/pixys/sepolicy
+
+rm -rf packages/apps/GameSpace
+git clone https://github.com/AOSP-Sweet-Trees/packages_apps_GameSpace packages/apps/GameSpace
+
+rm -rf frameworks/opt/telephony
+git clone https://github.com/Project-Awaken/android_frameworks_opt_telephony frameworks/opt/telephony
+
+rm -rf vendor/certification
+git clone https://gitlab.com/pure-soul-kk/vendor_certification vendor/certification
+
