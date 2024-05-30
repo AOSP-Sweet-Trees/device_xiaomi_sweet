649c2a1811f60d72213e792bc8f0369413615506
K R I S H N A <krishnakripa34567@gmail.com>
2024-05-30 18:51:26 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,15 +4,15 @@ git clone -b lineage https://github.com/AOSP-Sweet-Trees/device_xiaomi_sm6150-co
 
 # Vendor Tree
 rm -rf vendor/xiaomi
-git clone --depth=1 -b UNO https://github.com/ProjectElixir-Devices/vendor_xiaomi_sweet vendor/xiaomi/sweet
-git clone --depth=1 -b UNO https://github.com/ProjectElixir-Devices/vendor_xiaomi_sm6150-common-sweet vendor/xiaomi/sm6150-common
+git clone --depth=1 https://github.com/pure-soul-kk/vendor_xiaomi_sweet vendor/xiaomi/sweet
+git clone --depth=1 https://github.com/pure-soul-kk/vendor_xiaomi_sm6150-common vendor/xiaomi/sm6150-common
 
 # Miui Camera
 git clone --depth=1 https://github.com/pure-soul-kk/vendor_xiaomi_sweet-miuicamera vendor/xiaomi/sweet-miuicamera
 
 # Xiaomi Hardware
 rm -rf hardware/xiaomi
-git clone https://github.com/ProjectElixir-Devices/hardware_xiaomi hardware/xiaomi
+git clone https://github.com/pure-soul-kk/hardware_xiaomi hardware/xiaomi
 
 # Kernel Tree
 rm -rf kernel/xiaomi
